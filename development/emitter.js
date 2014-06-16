f42f1580b52612c571d2f129a6ae0e91a566634b
John Adams <github@borilla.co.uk>
2014-06-17 00:09:28 +0100

    Added basic 'on', 'trigger' and 'off' methods

--- development/emitter.js
+++ development/emitter.js
@@ -1,6 +1,43 @@
 var Emitter = (function() {
 
 	function Emitter() {
+		this.listeners = {};
+	}
+
+	Emitter.prototype.on = function(event, listener) {
+		var listeners = this.listeners[event] || (this.listeners[event] = []);
+		listeners.push(listener);
+	}
+
+	var slice = Array.prototype.slice;
+
+	Emitter.prototype.trigger = function(event) {
+		var listeners = this.listeners[event];
+		if (listeners && listeners.length) {
+			var args = slice.call(arguments, 1);
+			for (var i = 0, l = listeners.length; i < l; ++i) {
+				var listener = listeners[i];
+				listener.apply(null, args);
+			}
+		}
+	}
+
+	Emitter.prototype.off = function(event, listener) {
+		if (!event) {
+			this.listeners = {};
+		}
+		else if (!listener) {
+			delete this.listeners[event];
+		}
+		else {
+			var listeners = this.listeners[event];
+			if (listeners) {
+				var i = listeners.indexOf(listener);
+				if (i != -1) {
+					listeners = listeners.splice(i, 1);
+				}
+			}
+		}
 	}
 
 	return Emitter;
--- qunit/emitter.spec.js
+++ qunit/emitter.spec.js
@@ -10,4 +10,91 @@
 	test('should exist and be a function', function() {
 		equal(typeof(Emitter), 'function', 'Emitter is a function');
 	});
+
+	test('should create an instance of en emitter', function() {
+		var emitter = new Emitter();
+		ok(emitter instanceof Emitter, 'Object is an instance of Emitter');
+	});
+}());
+
+(function() {
+	module('Emitter instance');
+
+	var emitter;
+	var listener1, listener2;
+
+	QUnit.testStart(function() {
+		emitter = new Emitter();
+		listener1 = sinon.stub();
+		listener2 = sinon.stub();
+		listener3 = sinon.stub();
+	});
+
+	QUnit.testDone(function() {
+	});
+
+	test('should be able to add a listener to an event', function() {
+		emitter.on('event', listener1);
+		expect(0);
+	});
+
+	test('should be able to trigger listener to an event', function() {
+		emitter.on('event', listener1);
+		emitter.trigger('event');
+		ok(listener1.calledOnce, 'Listener was called once');
+	});
+
+	test('should not fail if we trigger an event with no listeners', function() {
+		emitter.trigger('xxx');
+		expect(0);
+	});
+
+	test('should be able to trigger multiple listeners', function() {
+		emitter.on('event', listener1);
+		emitter.on('event', listener2);
+		emitter.trigger('event');
+		ok(listener1.calledOnce, 'Listener1 was called once');
+		ok(listener2.calledOnce, 'Listener1 was called once');
+	});
+
+	test('should call listeners with arguments sent to trigger', function() {
+		var arg1 = 'john';
+		var arg2 = {};
+		var arg3 = [];
+		emitter.on('event', listener1);
+		emitter.on('event', listener2);
+		emitter.trigger('event', arg1, arg2, arg3);
+		ok(listener1.calledWith(arg1, arg2, arg3), 'Listener1 was called with expected arguments');
+		ok(listener2.calledWith(arg1, arg2, arg3), 'Listener2 was called with expected arguments');
+	});
+
+	test('should be able to remove a single listener', function() {
+		emitter.on('event', listener1);
+		emitter.on('event', listener2);
+		emitter.on('event', listener3);
+		emitter.off('event', listener2);
+		emitter.trigger('event');
+		ok(listener1.calledOnce, 'Listener1 was called once');
+		ok(listener2.notCalled, 'Listener2 was not called');
+		ok(listener3.calledOnce, 'Listener3 was called once');
+	});
+
+	test('should be able to remove all listeners to an event', function() {
+		emitter.on('event', listener1);
+		emitter.on('event', listener2);
+		emitter.off('event');
+		emitter.trigger('event');
+		ok(listener1.notCalled, 'Listener1 was not called');
+		ok(listener2.notCalled, 'Listener2 was not called');
+	});
+
+	test('should be able to remove all listeners to all events', function() {
+		emitter.on('event1', listener1);
+		emitter.on('event2', listener2);
+		emitter.off();
+		emitter.trigger('event1');
+		emitter.trigger('event2');
+		ok(listener1.notCalled, 'Listener1 was not called');
+		ok(listener2.notCalled, 'Listener2 was not called');
+	});
 }());
